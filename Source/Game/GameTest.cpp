f4760f5b544326cb356b93a9425e1a7d83d90a9d
Khalil Hajlaoui <98230912+KhajlaouiBdeB@users.noreply.github.com>
2026-06-16 14:38:41 -0400

    [Code] Add GameTest

---
+++ Source/Game/GameTest.cpp
@@ -0,0 +1,25 @@
+#include "GameTest.h"
+
+namespace Core
+{
+    GameTest::GameTest()
+    = default;
+
+    GameTest::~GameTest()
+    = default;
+
+    void GameTest::handleInput()
+    {
+        
+    }
+
+    void GameTest::update()
+    {
+        
+    }
+
+    void GameTest::render()
+    {
+        
+    }
+}
---
+++ Source/Game/GameTest.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../Core/GameBase.h"
+
+namespace Core
+{
+    class GameTest : public GameBase
+    {
+    public:
+        GameTest();
+        ~GameTest() override;
+
+        void handleInput() override;
+        void update() override;
+        void render() override;
+    }; 
+}
