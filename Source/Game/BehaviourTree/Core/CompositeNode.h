875d84e9c40efd7a39c0aa54cfcf7cf61f5e2837
Khalil Hajlaoui <98230912+KhajlaouiBdeB@users.noreply.github.com>
2026-06-23 15:54:07 -0400

    [Code] Add CompositeNode classe

---
+++ Source/Game/BehaviourTree/Core/CompositeNode.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+
+#include "Node.h"
+
+namespace BehaviourTree
+{
+    class CompositeNode : public Node
+    {
+    public:
+        virtual ~CompositeNode() 
+        {
+            for (Node* node : _nodes) 
+                delete node;
+        }
+
+    protected:
+        std::vector<Node*> _nodes;
+        CompositeNode() override = default; 
+
+    };
+}
