dbbfc04b99bbd5c2772b141ed7f0b889eb6e80a6
Khalil Hajlaoui <98230912+KhajlaouiBdeB@users.noreply.github.com>
2026-06-26 16:29:02 -0400

    [Code] Add builders

---
+++ Source/Game/BehaviourTree/Builders.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Core/BehaviourTree.h"
+
+namespace BehaviourTree
+{
+    class Builders
+    {
+        // Testing case for the tree, can be replaced with a more complex tree structure
+        static BehaviourTree* TestSequence()
+        {
+            auto bt = new BehaviourTree();
+
+            auto sn = new Sequence();
+            sn->add(new DummySuccess());
+            sn->add(new DummyFail());
+            sn->add(new DummySuccess());
+
+            bt->setRootNode(sn);
+
+            return bt;
+        }
+
+        static BehaviourTree* TestSelector()
+        {
+            auto bt = new BehaviourTree();
+
+            auto sn = new Selector();
+            sn->add(new DummySuccess());
+            sn->add(new DummyFail());
+            sn->add(new DummyFail());
+
+            bt->setRootNode(sn);
+
+            return bt;
+        }
+
+        static BehaviourTree* TestComposites()
+        {
+            auto bt = new BehaviourTree();
+            auto sln = new Selector();
+            auto sqn = new Sequence();
+
+            sqn->add(new DummySuccess());
+            sqn->add(sln);
+            sqn->add(new DummySuccess());
+
+            sln->add(new DummyFail());
+            sln->add(new DummyFail());
+            sln->add(new DummyFail());
+
+            bt->setRootNode(sn);
+
+            return bt;
+        }
+    };
+}
--- Source/Game/BehaviourTree/Core/BehaviourTree.h
+++ Source/Game/BehaviourTree/Core/BehaviourTree.h
@@ -3,9 +3,6 @@
 #include "BlackBoard.h"
 #include "Node.h"
 
-#include "../Composites.h"
-#include "../DummyLeaves.h"
-
 namespace BehaviourTree
 {
     class BehaviourTree
@@ -14,20 +11,20 @@ namespace BehaviourTree
         Node* _rootNode = nullptr;
         bool _complete = false;
 
-        BehaviorTree::BlackBoard _blackboard;
+        BlackBoard _blackboard;
 
     public:
-        BehaviourTree()
-        {
-            //TestSequence();
-            //TestSelector();
-            TestComposites();
-        };
+        BehaviourTree() = default;
 
         ~BehaviourTree()
         {
             delete _rootNode;
             _rootNode = nullptr;
+        }
+
+        void setRootNode(Node* rootNode)
+        {
+            _rootNode = rootNode;
         }
 
         NodeState tick()
@@ -38,47 +35,10 @@ namespace BehaviourTree
             return NodeState::FAILURE;
         }
 
-        bool isComplete()
+        bool isComplete() const
         {
             return _complete;
         };
 
-    private:
-        // Testing case for the tree, can be replaced with a more complex tree structure
-        void TestSequence()
-        {
-            auto sn = new Sequence();
-            sn->add(new DummySuccess());
-            sn->add(new DummyFail());
-            sn->add(new DummySuccess());
-
-            _rootNode = sn;
-        }
-
-        void TestSelector()
-        {
-            auto sn = new Selector();
-            sn->add(new DummySuccess());
-            sn->add(new DummyFail());
-            sn->add(new DummyFail());
-
-            _rootNode = sn;
-        }
-
-        void TestComposites()
-        {
-            auto sln = new Selector();
-            auto sqn = new Sequence();
-
-            sqn->add(new DummySuccess());
-            sqn->add(sln);
-            sqn->add(new DummySuccess());
-
-            sln->add(new DummyFail());
-            sln->add(new DummyFail());
-            sln->add(new DummyFail());
-
-            _rootNode = sqn;
-        }
     };
 }
