123747d5309eb9e0ba4cb2fefa310db0c050fd71
Khalil Hajlaoui <98230912+KhajlaouiBdeB@users.noreply.github.com>
2026-06-26 14:45:12 -0400

    [Code] Add Dummy Leaves

---
+++ Source/Game/BehaviourTree/DummyLeaves.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../../Core/Globals.h"
+#include "Core/LeafNode.h"
+
+namespace BehaviourTree
+{
+    class DummySuccess : public LeafNode
+    {
+    public:
+        NodeState tick() override
+        {
+            Core::log("DummySuccess Leaf");
+            return NodeState::SUCCESS;
+        }
+    };
+
+
+    class DummyFail : public LeafNode
+    {
+    public:
+        NodeState tick() override
+        {
+            Core::log("DummyFail Leaf");
+            return NodeState::FAILURE;
+        }
+    };
+
+    class DummyRunning : public LeafNode
+    {
+    public:
+        NodeState tick() override
+        {
+            Core::log("DummyRunning Leaf");
+            return NodeState::RUNNING;
+        }
+    };
+}
